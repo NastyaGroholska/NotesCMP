8c65a68ed148f8d7893976271b87454973135b01
Anastasiia <anastasiiahrokholska@gmail.com>
2025-08-14 11:27:17 -0700

    Added photo copy ios implementation

--- feature/create-note/src/iosMain/kotlin/com/ahrokholska/create_note/di/Module.ios.kt
+++ feature/create-note/src/iosMain/kotlin/com/ahrokholska/create_note/di/Module.ios.kt
@@ -1,10 +1,16 @@
 package com.ahrokholska.create_note.di
 
 import com.ahrokholska.create_note.domain.useCase.Copier
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import org.koin.core.module.Module
-import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 actual val platformModule: Module = module {
-    factoryOf(::Copier)
+    factory {
+        Copier(
+            repository = get(),
+            ioDispatcher = Dispatchers.IO
+        )
+    }
 }
--- feature/create-note/src/iosMain/kotlin/com/ahrokholska/create_note/domain/useCase/CopyAndUpdateImageUseCase.ios.kt
+++ feature/create-note/src/iosMain/kotlin/com/ahrokholska/create_note/domain/useCase/CopyAndUpdateImageUseCase.ios.kt
@@ -1,6 +1,67 @@
 package com.ahrokholska.create_note.domain.useCase
 
-actual class Copier {
-    actual fun doWork() {
+import com.ahrokholska.api.NotesRepository
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+actual class Copier(
+    private val repository: NotesRepository,
+    private val ioDispatcher: CoroutineDispatcher,
+) {
+    @OptIn(DelicateCoroutinesApi::class, ExperimentalForeignApi::class, ExperimentalTime::class)
+    actual fun doWork(uri: String) {
+        GlobalScope.launch {
+            withContext(ioDispatcher) {
+                try {
+
+                    val url = NSURL(string = uri)
+                    val fm = NSFileManager()
+                    val directoryURL = fm.URLForDirectory(
+                        directory = NSDocumentDirectory,
+                        inDomain = NSUserDomainMask,
+                        appropriateForURL = null,
+                        create = true,
+                        error = null
+                    )
+                    val fileName = "${Clock.System.now().toEpochMilliseconds()}${url.pathExtension}"
+                    var newURL = directoryURL?.URLByAppendingPathComponent(
+                        pathComponent = "$IMAGE_PATH/"
+                    ) ?: throw Exception("Unable to create dir URL")
+
+                    newURL.path?.let { path ->
+                        if (!fm.fileExistsAtPath(path)) {
+                            println("dir doesn't exist")
+                            fm.createDirectoryAtPath(
+                                path = path,
+                                withIntermediateDirectories = true,
+                                attributes = null,
+                                error = null
+                            )
+                        }
+                    } ?: throw Exception("path is null")
+
+                    newURL = newURL.URLByAppendingPathComponent(fileName)
+                        ?: throw Exception("Unable to create file URL")
+                    fm.copyItemAtURL(url, newURL, null)
+                    repository.updateImage(uri, "$IMAGE_PATH/$fileName")
+                } catch (e: Exception) {
+                    print("ERROR: $e")
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val IMAGE_PATH = "images"
     }
 }
